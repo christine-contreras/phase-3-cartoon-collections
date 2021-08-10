aecd31b9766ae9f3563d41603a81b7cf4744e8f7
Christine Contreras <christineec23@yahoo.com>
2021-08-10 16:54:13 -0500

    working

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarfs)
   # Your code here
+  dwarfs.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)
   # Your code here
+  calls.collect do |call|
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)
   # Your code here
+  words.any?{ |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(words)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  words.find do |word|
+    cheese_types.include?(word)
+  end
+
 end
